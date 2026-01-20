7322e0de1f703d698764b4352fb6d39120570781
Abdulai059 <abdulaiosman8080@gmail.com>
2026-01-20 06:00:41 +0000

    added blog

--- src/components/pages/BlogPost.jsx
+++ src/components/pages/BlogPost.jsx
@@ -17,7 +17,7 @@ export default function BlogPostCards() {
 
     return (
         <div className="max-w-7xl mx-auto px-4 py-12">
-            <h2 className="text-3xl font-semibold mb-8">All Blog Posts</h2>
+            <h2 className="text-3xl font-medium mb-8">All Blog Posts</h2>
 
             <div className="grid grid-cols-1 md:grid-cols-2 lg:grid-cols-3 gap-8">
                 {posts.map((post) => (
